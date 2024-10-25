9c8179cef191d978ce5bf5d2705762c019f74809
akberofh <sd85bejal@code.edu.az>
2024-10-25 18:34:41 +0400

    first commisfvtfff

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -23,31 +23,29 @@ const userControlAuth = async (req, res, next) => {
 };
 
 // Admin kontrolünde JWT token doğrulaması da yapıldı
+const adminControlAuth = async (req, res, next) => {
+    let token;
 
+    token = req.cookies.jwt;
 
-const adminControlAuth = (req, res, next) => {
-  const token = req.cookies.jwt; // JWT'nin cookie'den alındığından emin olun
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  if (!token) {
-    return res.status(401).json({ message: 'Yetkilendirme hatası' });
-  }
+            req.user = await User.findById(decoded.userId).select('-password');
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: 'Geçersiz token' });
-    
-    // Doğrulanan kullanıcı bilgisini `req.user` içine ekleyin
-    req.user = decoded;
-
-    // Kullanıcı `admin` değilse erişimi engelleyin
-    if (req.user.userType !== 'admin') {
-      return res.status(403).json({ message: 'Forbidden - admin deyilsen' });
+            if (req.user && req.user.userType === 'admin') {
+                next();
+            } else {
+                res.status(403).json({ message: 'Forbidden - admin deyilsen' });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(401).json({ message: 'Unauthorized - invalid token' });
+        }
+    } else {
+        res.status(401).json({ message: 'Unauthorized - token not found' });
     }
-
-    next();
-  });
 };
-
-export default adminControlAuth;
-
 
 export { userControlAuth, adminControlAuth };
