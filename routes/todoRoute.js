695f85cb00e24d79d785e19726774df7add482dc
akberofh <sd85bejal@code.edu.az>
2024-10-26 23:27:10 +0400

    fxsfsdesf

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { userControlAuth } from "../middleware/authMiddleware.js";
+import { adminControlAuth, userControlAuth } from "../middleware/authMiddleware.js";
 import { addUserTodo, getUserTodos, deleteUserTodo,updateTodoProfile } from "../controllers/todoController.js";
 import upload from '../middleware/uploadMiddleware.js';
 
 
 const router = express.Router();
 
-router.post('/',  upload.single('photo'), userControlAuth, addUserTodo);
-router.get('/', userControlAuth, getUserTodos);
+router.post('/',  upload.single('photo'), userControlAuth, adminControlAuth, addUserTodo);
+router.get('/', userControlAuth,  adminControlAuth, getUserTodos);
 router.delete('/:id', userControlAuth, deleteUserTodo);
 router.route('/profile')
 .put(userControlAuth, upload.single('photo'), updateTodoProfile);
